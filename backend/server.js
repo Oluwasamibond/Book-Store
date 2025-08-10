7aa61c00278ac3d3bf3d2df70633b23b728a68be
Oluwasamibond <Oluwasamibond@gmail.com>
2025-08-10 23:09:08 +0100

    sixth commit

--- backend/server.js
+++ backend/server.js
@@ -331,10 +331,9 @@ app.get("/api/search", async (req, res) => {
   }
 });
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) => {
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }

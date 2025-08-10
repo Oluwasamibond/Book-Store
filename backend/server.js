2be22b40e1246e574f302bb1a7c45d0ff59cfde0
Oluwasamibond <Oluwasamibond@gmail.com>
2025-08-11 00:38:58 +0100

    express install

--- backend/server.js
+++ backend/server.js
@@ -331,12 +331,11 @@ app.get("/api/search", async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  // Catch-all for SPA
-  app.get('/:path(*)', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
